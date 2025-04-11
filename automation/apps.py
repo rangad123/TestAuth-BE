c401c7e797c8a8dd27821273b9342e37231013c2
Narendra <narendrasompalli02@gmail.com>
2025-04-11 17:42:25 +0530

    Updated .gitignore to ignore .exe files

--- automation/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from .tasks import start_keep_alive_thread  # Import the function
-
-class AutomationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'automation'
-
-    def ready(self):
-        start_keep_alive_thread() 
